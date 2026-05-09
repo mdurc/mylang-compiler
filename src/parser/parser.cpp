mdurc/mylang-compiler

commit d17cf297c4f0941f77453cae5fab4b5785f11ec2

refactored AST nodes to favor trivially-destructible types allocated on the arena; fixed major memory leaks

src/parser/parser.cpp
@@ -148,7 +148,7 @@ AstPtr Parser::parse_enum_decl() {
     throw ParsePanic();
   }
 
-  EnumDeclPtr enum_node = _alloc(EnumDeclNode, name_tok, scope_id, name_ident, std::vector<EnumVariantAST>{});
+  EnumDeclPtr enum_node = _alloc(EnumDeclNode, name_tok, scope_id, name_ident, std::span<EnumVariantAST>{});
   if (!m_symtab->declare_enum(enum_name, enum_node, scope_id)) {
     m_logger->report(Diag::DuplicateDeclaration(name_tok->get_span(), "enum " + std::string(enum_name)));
     throw ParsePanic();
@@ -199,7 +199,7 @@ AstPtr Parser::parse_enum_decl() {
           m_symtab->declare_struct(allocated_name, payload_struct, scope_id);
         }
 
-        ast_variants.push_back({variant_ident, payload_fields});
+        ast_variants.push_back({variant_ident, freeze(payload_fields)});
         type_variants.push_back({variant_name, payload_type, current_tag++});
 
       } while (match(TokenType::COMMA) && advance());
@@ -211,8 +211,8 @@ AstPtr Parser::parse_enum_decl() {
   }
   m_symtab->exit_scope();
 
-  sym_enum_type->as<Type::Enum>().variants = std::move(type_variants);
-  enum_node->variants = std::move(ast_variants);
+  sym_enum_type->as<Type::Enum>().variants = freeze(type_variants);
+  enum_node->variants = freeze(ast_variants);
   enum_node->type = sym_enum_type;
 
   return enum_node;
@@ -255,32 +255,33 @@ AstPtr Parser::parse_struct_decl() {
     sym_struct_type = m_symtab->declare_type(type_name, struct_type, scope_id);
 
     // now link the struct declaration node to the symbol table for the IR
-    struct_node = _alloc(StructDeclNode, name_tok, scope_id, sym_struct_type, std::vector<StructFieldPtr>{});
+    struct_node = _alloc(StructDeclNode, name_tok, scope_id, sym_struct_type, std::span<StructFieldPtr>{});
     m_symtab->declare_struct(type_name, struct_node, scope_id);
   }
 
   if (is_opaque_decl) {
     advance();
-    return _alloc(StructDeclNode, name_tok, scope_id, sym_struct_type, std::vector<StructFieldPtr>{});
+    return _alloc(StructDeclNode, name_tok, scope_id, sym_struct_type, std::span<StructFieldPtr>{});
   }
 
   sym_struct_type->set_complete(true);
 
+  std::vector<StructFieldPtr> temp_fields;
   m_symtab->enter_scope(); // new scope for the contents
   try {
     _consume(TokenType::LBRACE);
-
     if (!match(TokenType::RBRACE)) {
       do {
-        struct_node->fields.push_back(parse_struct_field());
+        temp_fields.push_back(parse_struct_field());
       } while (match(TokenType::COMMA) && advance());
     }
-
+    struct_node->fields = freeze(temp_fields);
     _consume(TokenType::RBRACE);
   } catch (const ParsePanic&) {
     m_symtab->exit_scope();
     synchronize();
     if (match(TokenType::RBRACE)) advance();
+    struct_node->fields = freeze(temp_fields);
     return struct_node; // return partially-built struct
 
   }
@@ -377,7 +378,7 @@ FuncDeclPtr Parser::parse_function_decl() {
   }
 
   // it must be at the global scope (no struct methods)
-  Type ft(Type::Function(std::move(ft_params), return_t.second), 0);
+  Type ft(Type::Function(freeze(ft_params), return_t.second), 0);
   std::string_view ft_type_str = m_arena->make_string(ft.to_string());
 
   // do not redeclare function families
@@ -402,7 +403,7 @@ FuncDeclPtr Parser::parse_function_decl() {
   }
 
   return _alloc(FunctionDeclNode, func_tok, m_symtab->current_scope(), is_ext, name_ident,
-              std::move(params_vec), return_t.first, return_t.second, body_block);
+              freeze(params_vec), return_t.first, return_t.second, body_block);
 }
 
 // <Param> ::= <TypePrefix>? Identifier ':' <Type>
@@ -584,7 +585,7 @@ StmtPtr Parser::parse_if_stmt() {
       StmtPtr else_if_stmt = parse_if_stmt();
       const Token* else_if_tok = else_if_stmt->token;
       else_branch = _alloc(BlockNode, else_if_tok, m_symtab->current_scope(),
-                         std::vector<StmtPtr>{else_if_stmt});
+                         freeze(std::vector<StmtPtr>{else_if_stmt}));
     } else {
       // else block
       else_branch = parse_block(true);
@@ -711,7 +712,7 @@ StmtPtr Parser::parse_switch_stmt() {
   }
 
   _consume(TokenType::RBRACE);
-  return _alloc(SwitchStmtNode, switch_tok, m_symtab->current_scope(), expression, std::move(cases));
+  return _alloc(SwitchStmtNode, switch_tok, m_symtab->current_scope(), expression, freeze(cases));
 }
 
 // <PrintStmt> ::= 'print'  <Expr> ( ',' <Expr> )*
@@ -724,7 +725,7 @@ StmtPtr Parser::parse_print_stmt() {
     exprs.push_back(parse_expression());
   } while (match(TokenType::COMMA) && advance());
   _consume(TokenType::SEMICOLON);
-  return _alloc(PrintStmtNode, print_tok, m_symtab->current_scope(), std::move(exprs));
+  return _alloc(PrintStmtNode, print_tok, m_symtab->current_scope(), freeze(exprs));
 }
 
 // == Expression Parsing ==
@@ -785,7 +786,7 @@ StmtPtr Parser::parse_error_stmt() {
   } while (match(TokenType::COMMA) && advance());
   _consume(TokenType::SEMICOLON);
 
-  return _alloc(ErrorStmtNode, error_tok, m_symtab->current_scope(), std::move(exprs));
+  return _alloc(ErrorStmtNode, error_tok, m_symtab->current_scope(), freeze(exprs));
 }
 
 // <ExitStmt> ::= 'exit' <Expr>?
@@ -808,7 +809,6 @@ BlockPtr Parser::parse_block(bool create_scope) {
   const Token* lbrace_tok = current();
 
   std::vector<StmtPtr> statements_vec;
-
   if (create_scope) m_symtab->enter_scope();
   try {
     _consume(TokenType::LBRACE);
@@ -820,11 +820,11 @@ BlockPtr Parser::parse_block(bool create_scope) {
     if (create_scope) m_symtab->exit_scope();
     synchronize();
     if (match(TokenType::RBRACE)) advance();
-    return _alloc(BlockNode, lbrace_tok, m_symtab->current_scope(), std::move(statements_vec)); // partially error'd block
+    return _alloc(BlockNode, lbrace_tok, m_symtab->current_scope(), freeze(statements_vec)); // partially error'd block
   }
   if (create_scope) m_symtab->exit_scope();
 
-  return _alloc(BlockNode, lbrace_tok, m_symtab->current_scope(), std::move(statements_vec));
+  return _alloc(BlockNode, lbrace_tok, m_symtab->current_scope(), freeze(statements_vec));
 }
 
 // <Stmt> ::= ... | 'asm' <Block> ';'
@@ -881,7 +881,7 @@ StmtPtr Parser::parse_asm_block() {
   _consume(TokenType::RBRACE);
   _consume(TokenType::SEMICOLON);
 
-  return _alloc(AsmBlockNode, asm_tok, m_symtab->current_scope(), asm_body_str);
+  return _alloc(AsmBlockNode, asm_tok, m_symtab->current_scope(), m_arena->make_string(asm_body_str));
 }
 
 // Expression Hierarchy
@@ -1107,8 +1107,7 @@ ExprPtr Parser::parse_postfix() {
         ExprPtr index_expr = parse_expression();
 
         _consume(TokenType::RBRACK);
-        expr = _alloc(ArrayIndexNode, op_tok, m_symtab->current_scope(), expr,
-                    index_expr);
+        expr = _alloc(ArrayIndexNode, op_tok, m_symtab->current_scope(), expr, index_expr);
         break;
       }
       case TokenType::LPAREN: {
@@ -1117,8 +1116,7 @@ ExprPtr Parser::parse_postfix() {
         std::vector<ArgPtr> args_vec = parse_args();
         _consume(TokenType::RPAREN);
 
-        expr = _alloc(FunctionCallNode, op_tok, m_symtab->current_scope(), expr,
-                    std::move(args_vec));
+        expr = _alloc(FunctionCallNode, op_tok, m_symtab->current_scope(), expr, freeze(args_vec));
         break;
       }
       case TokenType::DOT: {
@@ -1239,7 +1237,7 @@ Type* Parser::parse_type() {
     Type* ret_type = parse_type();
 
     // functions are declared at scope 0
-    Type func_type(Type::Function(std::move(param_types), ret_type), 0);
+    Type func_type(Type::Function(freeze(param_types), ret_type), 0);
     std::string_view name = m_arena->make_string(func_type.to_string());
     Type* t = m_symtab->lookup<Type>(name, 0);
     if (t == nullptr) {
@@ -1318,7 +1316,7 @@ ExprPtr Parser::parse_primitive_literal() {
                   tok->get_float_val());
     case TokenType::STRING_LITERAL:
       return _alloc(StringLiteralNode, tok, m_symtab->current_scope(),
-                  tok->get_string_val());
+          m_arena->make_string(tok->get_string_val()));
     case TokenType::CHAR_LITERAL:
       return _alloc(CharLiteralNode, tok, m_symtab->current_scope(),
                   tok->get_int_val());
@@ -1365,7 +1363,7 @@ ExprPtr Parser::parse_struct_literal(StructDeclPtr struct_decl) {
   _consume(TokenType::RBRACE);
 
   return _alloc(StructLiteralNode, type_name_tok, m_symtab->current_scope(),
-              struct_decl, std::move(initializers_vec));
+              struct_decl, freeze(initializers_vec));
 }
 
 // <EnumLiteral> ::= Identifier '::' Identifier ('{' ( Identifier '=' <Expr> ( ',' Identifier '=' <Expr> )* )? '}')?
@@ -1393,7 +1391,7 @@ ExprPtr Parser::parse_enum_literal(EnumDeclPtr enum_decl) {
     }
     _consume(TokenType::RBRACE);
   }
-  return _alloc(EnumLiteralNode, enum_tok, m_symtab->current_scope(), enum_decl, variant_ident, std::move(initializers));
+  return _alloc(EnumLiteralNode, enum_tok, m_symtab->current_scope(), enum_decl, variant_ident, freeze(initializers));
 }
 
 // <NewExpr> ::= 'new' '<'<TypePrefix>? <Type>'>' ( '['<Expr>']' | '('(<StructLiteral> | <Expr>)?')' )

.gitignore
@@ -7,3 +7,4 @@ compiler_build_files/
 tfile.sn
 tfile.sn.*
 DATA.txt
+LEAKS.txt

Makefile
@@ -90,9 +90,12 @@ test: compile_test_macos
 test_linux: compile_test_linux
 	./$(TFILE).exe
 
+test_leaks: $(PROGRAM)
+	leaks --atExit -- compiler_build_files/mycompiler --tokens tfile.sn tfile.sn.tokens --ast tfile.sn tfile.sn.ast --symtab tfile.sn tfile.sn.symtab --ir tfile.sn tfile.sn.ir --asm tfile.sn tfile.sn.asm --json tfile.sn tfile.sn.json --exe tfile.sn tfile.sn.exe > LEAKS.txt 2>&1
+
 clean:
 	rm -rf $(MYLIB)
-	rm -f $(TFILE).* a.out DATA.txt .DS_Store
+	rm -f $(TFILE).* a.out DATA.txt LEAKS.txt .DS_Store
 
 .PHONY: all clean \
 	update_test  \

src/checker/typechecker.cpp
@@ -583,7 +583,8 @@ void TypeChecker::visit(FunctionDeclNode& node) {
 
   /* confirm that this function exists in the symbol table */
   size_t scope = node.scope_id;
-  Type fn_type(Type::Function(std::move(param_infos), node.return_type), scope);
+  // we don't need to freeze the param_infos because this type is only used locally here for a type lookup
+  Type fn_type(Type::Function(param_infos, node.return_type), scope);
   Type* t = m_symtab->lookup<Type>(fn_type.to_string(), scope);
   _assert(t != nullptr, "Parser should declare the func type");
 

src/codegen/ir/ir_visitor.cpp
@@ -130,7 +130,7 @@ void IrVisitor::visit(BoolLiteralNode& node) {
 void IrVisitor::visit(StringLiteralNode& node) {
   /* backend will interpret the string
       and assume that it is PTR_SIZE */
-  m_last_expr_operand = node.value;
+  m_last_expr_operand = std::string(node.value);
 }
 
 void IrVisitor::visit(CharLiteralNode& node) {
@@ -909,7 +909,7 @@ void IrVisitor::visit(PrintStmtNode& node) {
 }
 
 void IrVisitor::visit(AsmBlockNode& node) {
-  m_ir_gen.emit_asm_block(node.body);
+  m_ir_gen.emit_asm_block(std::string(node.body));
 }
 
 // R-value access: x = arr[i]

src/json_export/json_exporter.cpp
@@ -63,7 +63,7 @@ static void collect_hover(const AstPtr& node, json& hover, const SymTab* symtab)
   } else if (auto lit = dynamic_cast<const FloatLiteralNode*>(node)) {
     emit_hover(hover, node, std::to_string(lit->value), lit->expr_type);
   } else if (auto lit = dynamic_cast<const StringLiteralNode*>(node)) {
-    emit_hover(hover, node, lit->value, lit->expr_type);
+    emit_hover(hover, node, std::string(lit->value), lit->expr_type);
   } else if (auto lit = dynamic_cast<const BoolLiteralNode*>(node)) {
     emit_hover(hover, node, lit->value ? "true" : "false", lit->expr_type);
   } else if (auto lit = dynamic_cast<const NullLiteralNode*>(node)) {

src/memory/arena.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <span>
 #include <vector>
 
 class ArenaAllocator {
@@ -23,6 +24,15 @@ public:
   T* make(Args&&... args) {
     void* memory = allocate(sizeof(T), alignof(T));
     return new (memory) T(std::forward<Args>(args)...);
+  }
+
+  /* use a span to easily allow vector-like allocations that don't
+     need to be destructured to free any heap memory */
+  template <typename T>
+  std::span<T> allocate_array(size_t count) {
+    if (count == 0) return std::span<T>();
+    void* memory = allocate(count * sizeof(T), alignof(T));
+    return std::span<T>(static_cast<T*>(memory), count);
   }
 
   /* helper to create new strings in the arena */

src/parser/ast.h
@@ -4,7 +4,7 @@
 #include <memory>
 #include <optional>
 #include <string>
-#include <vector>
+#include <span>
 
 #include "../lexer/token.h"
 #include "types.h"
@@ -59,7 +59,7 @@ enum UnaryOperator { Negate, LogicalNot, Dereference, AddressOf, AddressOfMut };
     T value;                                 \
     Name(const Token* tok, size_t sc, T val) \
         : ExpressionNode(tok, sc),           \
-          value(std::move(val)) {}           \
+          value(val) {}                      \
     void accept(Visitor& v) override;        \
   };
 
@@ -84,7 +84,7 @@ DERIVE_ABSTRACT_NODE(StatementNode, AstNode)
 // == Literal Nodes ==
 LITERAL_NODE(IntegerLiteralNode, std::uint64_t)
 LITERAL_NODE(FloatLiteralNode, double)
-LITERAL_NODE(StringLiteralNode, std::string)
+LITERAL_NODE(StringLiteralNode, std::string_view)
 LITERAL_NODE(CharLiteralNode, std::uint64_t)
 LITERAL_NODE(BoolLiteralNode, bool)
 DERIVE_NODE(NullLiteralNode, ExpressionNode)
@@ -183,9 +183,9 @@ public:
 class FunctionCallNode : public ExpressionNode {
 public:
   ExprPtr callee;
-  std::vector<ArgPtr> arguments;
-  FunctionCallNode(const Token* tok, size_t sc, ExprPtr fn_callee, std::vector<ArgPtr> args)
-      : ExpressionNode(tok, sc), callee(fn_callee), arguments(std::move(args)) {}
+  std::span<ArgPtr> arguments;
+  FunctionCallNode(const Token* tok, size_t sc, ExprPtr fn_callee, std::span<ArgPtr> args)
+      : ExpressionNode(tok, sc), callee(fn_callee), arguments(args) {}
   void accept(Visitor& v) override;
 };
 
@@ -227,9 +227,9 @@ public:
 class StructLiteralNode : public ExpressionNode {
 public:
   StructDeclPtr struct_decl;
-  std::vector<StructFieldInitPtr> initializers;
-  StructLiteralNode(const Token* tok, size_t sc, StructDeclPtr struct_decl, std::vector<StructFieldInitPtr> inits)
-      : ExpressionNode(tok, sc), struct_decl(struct_decl), initializers(std::move(inits)) {}
+  std::span<StructFieldInitPtr> initializers;
+  StructLiteralNode(const Token* tok, size_t sc, StructDeclPtr struct_decl, std::span<StructFieldInitPtr> inits)
+      : ExpressionNode(tok, sc), struct_decl(struct_decl), initializers(inits) {}
   void accept(Visitor& v) override;
 };
 
@@ -237,9 +237,9 @@ class EnumLiteralNode : public ExpressionNode {
 public:
   EnumDeclPtr enum_decl;
   IdentPtr variant_name;
-  std::vector<StructFieldInitPtr> initializers; /* empty if no payload */
-  EnumLiteralNode(const Token* tok, size_t sc, EnumDeclPtr ed, IdentPtr vn, std::vector<StructFieldInitPtr> init)
-      : ExpressionNode(tok, sc), enum_decl(ed), variant_name(vn), initializers(std::move(init)) {}
+  std::span<StructFieldInitPtr> initializers; /* empty if no payload */
+  EnumLiteralNode(const Token* tok, size_t sc, EnumDeclPtr ed, IdentPtr vn, std::span<StructFieldInitPtr> init)
+      : ExpressionNode(tok, sc), enum_decl(ed), variant_name(vn), initializers(init) {}
   void accept(Visitor& v) override;
 };
 
@@ -270,9 +270,9 @@ public:
 
 class BlockNode : public StatementNode {
 public:
-  std::vector<StmtPtr> statements;
-  BlockNode(const Token* tok, size_t sc, std::vector<StmtPtr> stmts)
-      : StatementNode(tok, sc), statements(std::move(stmts)) {}
+  std::span<StmtPtr> statements;
+  BlockNode(const Token* tok, size_t sc, std::span<StmtPtr> stmts)
+      : StatementNode(tok, sc), statements(stmts) {}
   void accept(Visitor& v) override;
 };
 
@@ -343,10 +343,10 @@ public:
 class SwitchStmtNode : public StatementNode {
 public:
   ExprPtr expression;
-  std::vector<CasePtr> cases;
+  std::span<CasePtr> cases;
   SwitchStmtNode(const Token* tok, size_t sc, ExprPtr expr,
-                 std::vector<CasePtr> c)
-      : StatementNode(tok, sc), expression(expr), cases(std::move(c)) {}
+                 std::span<CasePtr> c)
+      : StatementNode(tok, sc), expression(expr), cases(c) {}
   void accept(Visitor& v) override;
 };
 
@@ -360,9 +360,9 @@ public:
 
 class PrintStmtNode : public StatementNode {
 public:
-  std::vector<ExprPtr> expressions;
-  PrintStmtNode(const Token* tok, size_t sc, std::vector<ExprPtr> expr)
-      : StatementNode(tok, sc), expressions(std::move(expr)) {}
+  std::span<ExprPtr> expressions;
+  PrintStmtNode(const Token* tok, size_t sc, std::span<ExprPtr> expr)
+      : StatementNode(tok, sc), expressions(expr) {}
   void accept(Visitor& v) override;
 };
 
@@ -392,9 +392,9 @@ public:
 
 class ErrorStmtNode : public StatementNode {
 public:
-  std::vector<ExprPtr> expressions;
-  ErrorStmtNode(const Token* tok, size_t sc, std::vector<ExprPtr> expr)
-      : StatementNode(tok, sc), expressions(std::move(expr)) {}
+  std::span<ExprPtr> expressions;
+  ErrorStmtNode(const Token* tok, size_t sc, std::span<ExprPtr> expr)
+      : StatementNode(tok, sc), expressions(expr) {}
   void accept(Visitor& v) override;
 };
 
@@ -409,9 +409,9 @@ public:
 
 class AsmBlockNode : public StatementNode {
 public:
-  std::string body;
-  AsmBlockNode(const Token* tok, size_t sc, std::string b)
-      : StatementNode(tok, sc), body(std::move(b)) {}
+  std::string_view body;
+  AsmBlockNode(const Token* tok, size_t sc, std::string_view b)
+      : StatementNode(tok, sc), body(b) {}
   void accept(Visitor& v) override;
 };
 
@@ -430,24 +430,24 @@ class StructDeclNode : public AstNode {
 public:
   Type* type; /* new type to be set after type creation in parser     */
               /* struct's (padded) size will be reflected in the type */
-  std::vector<StructFieldPtr> fields; /* only fields, no methods */
-  StructDeclNode(const Token* tok, size_t sc, Type* type, std::vector<StructFieldPtr> f)
-      : AstNode(tok, sc), type(type), fields(std::move(f)) {}
+  std::span<StructFieldPtr> fields; /* only fields, no methods */
+  StructDeclNode(const Token* tok, size_t sc, Type* type, std::span<StructFieldPtr> f)
+      : AstNode(tok, sc), type(type), fields(f) {}
   void accept(Visitor& v) override;
 };
 
 struct EnumVariantAST {
   IdentPtr name;
-  std::vector<StructFieldPtr> fields; /* empty if no payload */
+  std::span<StructFieldPtr> fields; /* empty if no payload */
 };
 
 class EnumDeclNode : public AstNode {
 public:
   IdentPtr name;
   Type* type; /* new type to be set after type creation in parser */
-  std::vector<EnumVariantAST> variants;
-  EnumDeclNode(const Token* tok, size_t sc, IdentPtr name, std::vector<EnumVariantAST> variants)
-      : AstNode(tok, sc), name(name), type(nullptr), variants(std::move(variants)) {}
+  std::span<EnumVariantAST> variants;
+  EnumDeclNode(const Token* tok, size_t sc, IdentPtr name, std::span<EnumVariantAST> variants)
+      : AstNode(tok, sc), name(name), type(nullptr), variants(variants) {}
   void accept(Visitor& v) override;
 };
 
@@ -465,14 +465,14 @@ class FunctionDeclNode : public AstNode {
 public:
   bool is_extern;
   IdentPtr name;
-  std::vector<ParamPtr> params;
+  std::span<ParamPtr> params;
   IdentPtr retvar_name; /* optional for u0 return type */
   Type* return_type;
   BlockPtr body; /* nullptr if this is an extern function */
   FunctionDeclNode(const Token* tok, size_t sc, bool is_ext, IdentPtr name,
-                   std::vector<ParamPtr> ps, IdentPtr rt_n,
+                   std::span<ParamPtr> ps, IdentPtr rt_n,
                    Type* rt, BlockPtr b)
-      : AstNode(tok, sc), is_extern(is_ext), name(name), params(std::move(ps)),
+      : AstNode(tok, sc), is_extern(is_ext), name(name), params(ps),
         retvar_name(rt_n), return_type(rt), body(b) {}
   void accept(Visitor& v) override;
 };

commit 3254401c2de8cc9b02f21ecd90b8acb16616ffbe

fixed lingering bug for struct field access due to forgetting to freeze a field.. broke in commit d17cf29

src/parser/parser.cpp
@@ -170,6 +170,7 @@ AstPtr Parser::parse_enum_decl() {
 
         std::vector<StructFieldPtr> payload_fields;
         Type* payload_type = nullptr;
+        StructDeclPtr payload_struct = nullptr;
 
         if (match(TokenType::LBRACE)) {
           _consume(TokenType::LBRACE);
@@ -195,11 +196,12 @@ AstPtr Parser::parse_enum_decl() {
 
           // register anonymous struct in symbol table
           m_symtab->declare_type(allocated_name, payload_type, scope_id);
-          StructDeclPtr payload_struct = _alloc(StructDeclNode, variant_tok, scope_id, payload_type, payload_fields);
+          payload_struct = _alloc(StructDeclNode, variant_tok, scope_id, payload_type, freeze(payload_fields));
           m_symtab->declare_struct(allocated_name, payload_struct, scope_id);
         }
 
-        ast_variants.push_back({variant_ident, freeze(payload_fields)});
+        // ast_variants.push_back({variant_ident, freeze(payload_fields)});
+        ast_variants.push_back({variant_ident, payload_struct ? payload_struct->fields : freeze(payload_fields)});
         type_variants.push_back({variant_name, payload_type, current_tag++});
 
       } while (match(TokenType::COMMA) && advance());